a12cabcee3baadf47e753904e8bede3171c6db76
Branko <Branko@192.168.2.60>
2016-11-17 12:41:28 +0200

    == Initial import ==

---
+++ HelloWorldGit/src/com/lazyweaver/gittest/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.lazyweaver.gittest;
+
+/**
+ * TODO - DOCUMENT ME
+ * 
+ * @author Branko (Nov 17, 2016)
+ *
+ *         <br>
+ * 		<b>History:</b> <br>
+ *         Nov 17, 2016 Branko created <br>
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
